c34df85b7337f892456852afc993cb6c2fc0a8fd
GLADI8R <danishmehmuda@gmail.com>
2021-05-24 15:37:47 +0530

    Add Home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,18 @@
+import { Fragment } from 'react';
+
+import '../../Sass/styles.scss';
+import './Home.scss';
 
 const Home = () => {
     return(
-        <div>Hi, I'm DANISH </div>
+        <Fragment>
+            <div className="container">
+                <div className="mainHead centered textWhite">Hi, I'm Danish</div>
+                <div className="desc centered textPink">Web Developer and Programming Enthusiast</div>
+                <h3 className="tot centered textWhite">Tools of Trade</h3>
+            </div>
+        </Fragment>
+        
     );
 };
 
--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -2,15 +2,16 @@ import { Fragment } from "react";
 import { Link } from 'react-router-dom';
 
 import'./Nav.scss';
+import '../../Sass/styles.scss';
 
 const NavBar = () => {
     return(
         <Fragment>
             <nav className="navbar">
                 <ul>
-                    <li><Link className="nav-item textWhite" to="/">Home</Link></li>
-                    <li><Link className="nav-item textWhite" to="/about">About</Link></li>
-                    <li><Link className="nav-item textWhite" to="/project">Project</Link></li>
+                    <li><Link className="nav-item textBlue" to="/">Home</Link></li>
+                    <li><Link className="nav-item textBlue" to="/about">About</Link></li>
+                    <li><Link className="nav-item textBlue" to="/project">Project</Link></li>
                 </ul>
             </nav>
         </Fragment>
