fa11d7fe00740f954511ccdc285b43a26060418a
GLADI8R <danishmehmuda@gmail.com>
2021-05-26 23:12:30 +0530

    Modify About Page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,21 +1,24 @@
 import './About.scss';
 import '../../Sass/styles.scss';
 import Danish from '../../Asset/images/Danish.jpg';
+import resume from '../../Asset/Resume/Danish_Resume.pdf';
 
 const About = () => {
     return(
         <div className="container centered textWhite">
             <h2 className="textPink mtop-4">About Me</h2>
             <div className="row">
-                <div className="column centered">
+                <div className="column col1 right">
                     <img src={Danish} alt="profile"></img>
                     <span className="caption">Danish Ahmed Mehmuda</span>
                 </div>
-                <div className="column centered about">
+                <div className="column col2 about">
                     <p>I am Electronics and Communication Engineering Undergraduate</p>
                     <p>I have good understanding of Data Structures and Algorithms</p>
                     <p>I am passionate about programming and have good skills on Web Development</p>
-
+                    <a href={resume} download>
+                        <span>My Resume</span>
+                    </a>
                 </div>
             </div>
         </div>
