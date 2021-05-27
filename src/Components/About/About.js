fd83a06c8d6d0bd6a1347ad24897aa8ffb1746ba
GLADI8R <danishmehmuda@gmail.com>
2021-05-27 15:30:03 +0530

    Modify About Page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="container centered textWhite">
             <h2 className="textPink mtop-4">About Me</h2>
             <div className="row">
-                <div className="column col1 right">
+                <div className="column col1 centered right">
                     <img src={Danish} alt="profile"></img>
                     <span className="caption">Danish Ahmed Mehmuda</span>
                 </div>
