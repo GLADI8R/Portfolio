b09566965751fc598d4cab8f1ab5b1ea8f12ea4b
GLADI8R <danishmehmuda@gmail.com>
2021-08-20 23:04:47 +0530

    Minor changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             <div className="container">
                 <div className="mainHead centered textWhite">Hi, I'm Danish</div>
                 <div className="desc centered textPink">Web Developer and Programming Enthusiast</div>
-                <h2 className="centered textWhite">Tools of Trade</h2>
+                <h2 className="centered textWhite">Skills and Tools</h2>
                 <Skills />
             </div>
         </Fragment>
