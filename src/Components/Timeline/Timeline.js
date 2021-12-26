d642772e3fd293e3e777e7c461be8a3132eaa77d
GLADI8R <danishmehmuda@gmail.com>
2021-12-26 10:33:06 +0530

    Modify timeline

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Timeline, TimelineItem} from 'vertical-timeline-component-for-react';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 
 import '../../Sass/styles.scss';
 import './Timeline.scss';
@@ -13,8 +13,16 @@ const TimeLine = () => {
             <h2 className="textPink">Timeline</h2>
             <Timeline lineColor={"#93c2db"} className="textWhite font" >
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
+                    key="12"
+                    dateText="September 2021"
+                >
+                    <p className="textWhite">Competitive Programming and DSA</p>
+                </TimelineItem>
+
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="11"
                     dateText="August 2021"
                 >
@@ -22,8 +30,8 @@ const TimeLine = () => {
                     <p className="textWhite sub">Participated in the open access JPMorgan Chase & Co. Virtual Experience Program with Forage</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="10"
                     dateText="June 2021"
                 >
@@ -31,32 +39,32 @@ const TimeLine = () => {
                     <p className="textWhite">Made Spotify UI Clone</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="9"
                     dateText="April 2021"
                 >
                     <p className="textWhite">Learning React</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="8"
                     dateText="March 2021"
                 >
                     <p className="textWhite">Made JS Medium</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="7"
                     dateText="Feb 2021"
                 >
                     <p className="textWhite">Made To-Do List</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="6"
                     dateText="Dec 2020"
                 >
@@ -64,24 +72,24 @@ const TimeLine = () => {
                     <p className="textWhite">Learn Nodejs</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="5"
                     dateText="Sept 2020"
                 >
                     <p className="textWhite">Learn HTML, CSS, JavaScript</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="4"
                     dateText="May 2020"
                 >
                     <p className="textWhite">Competitve Programming in C++</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="3"
                     dateText="Aug 2019 - Present"
                 >
@@ -89,8 +97,8 @@ const TimeLine = () => {
                     <p className="textWhite sub">CGPA: 8.31/10.0</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="2"
                     dateText="Aug 2005 - May 2019"
                 >
@@ -100,17 +108,17 @@ const TimeLine = () => {
                     <p className="textWhite sub">Class 12 Non-Medical Sciences, 88.8/100</p>
                 </TimelineItem>
 
-                <TimelineItem 
-                    dateInnerStyle={{ background: bgColor, color: textColor}}
+                <TimelineItem
+                    dateInnerStyle={{ background: bgColor, color: textColor }}
                     key="1"
                     dateText="14 Billion Years Ago..."
                 >
-                    <p className="textWhite sub"> Our whole universe was in a hot, dense state<br/>
-                        Then nearly fourteen billion years ago expansion started, wait <br/>
-                        The earth began to cool, the autotrophs began to drool<br/>
-                        Neanderthals developed tools<br/>
-                        We built a wall (we built the pyramids)<br/>
-                        Math, science, history, unraveling the mysteries<br/>
+                    <p className="textWhite sub"> Our whole universe was in a hot, dense state<br />
+                        Then nearly fourteen billion years ago expansion started, wait <br />
+                        The earth began to cool, the autotrophs began to drool<br />
+                        Neanderthals developed tools<br />
+                        We built a wall (we built the pyramids)<br />
+                        Math, science, history, unraveling the mysteries<br />
                         That all started with the big bang! </p>
                 </TimelineItem>
 
