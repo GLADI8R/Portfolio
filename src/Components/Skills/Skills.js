4b543643eb581dc1b2bea9257e3f392e653a2c8d
GLADI8R <danishmehmuda@gmail.com>
2022-11-16 21:14:03 +0530

    Add to skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -10,10 +10,13 @@ import bootstrap from '../../Asset/icons/bootstrap.svg';
 import sass from '../../Asset/icons/sass.png';
 import js from '../../Asset/icons/javascript.jpg';
 import node from '../../Asset/icons/node.svg';
+import java from '../../Asset/icons/java.svg';
+import spring from '../../Asset/icons/spring.svg';
 import npm from '../../Asset/icons/npm.png';
 import express from '../../Asset/icons/express.png';
 import git from '../../Asset/icons/git.png';
 import mongo from '../../Asset/icons/mongo.png';
+import postgre from '../../Asset/icons/postgresql.svg';
 import react from '../../Asset/icons/react.png';
 import flutter from '../../Asset/icons/flutter.svg';
 import dart from '../../Asset/icons/dart.svg';
@@ -26,22 +29,25 @@ const Skills = () => {
         <div id="tools">
             <section>
                 <SkillCard image={cpp} text="C++" />
+                <SkillCard image={java} text="Java" />
+                <SkillCard image={js} text="JS" />
+                <SkillCard image={react} text="React" />
+                <SkillCard image={spring} text="Spring Boot" />
+                <SkillCard image={node} text="NodeJs" />
+                <SkillCard image={express} text="Express" />
+                <SkillCard image={docker} text="Docker" />
+                <SkillCard image={kubernetes} text="Kubernetes" />
                 <SkillCard image={html} text="HTML" />
                 <SkillCard image={css} text="CSS" />
                 <SkillCard image={bootstrap} text="Bootstrap" />
                 <SkillCard image={sass} text="Sass" />
-                <SkillCard image={js} text="JS" />
-                <SkillCard image={node} text="NodeJs" />
                 <SkillCard image={npm} text="NPM" />
-                <SkillCard image={express} text="Express" />
                 <SkillCard image={git} text="Git" />
                 <SkillCard image={mongo} text="MongoDB" />
-                <SkillCard image={react} text="React" />
+                <SkillCard image={postgre} text = "PostgreSQL" />
                 <SkillCard image={flutter} text="Flutter" />
                 <SkillCard image={dart} text="Dart" />
                 <SkillCard image={illustrator} text="Illustrator" />
-                <SkillCard image={docker} text="Docker" />
-                <SkillCard image={kubernetes} text="Kubernetes" />
             </section>
         </div>
     );
